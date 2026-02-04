f1b5a1f208007dd7f5e899bfdc7f046c9239b8de
Miika Hakala <miika.hakala@mail.com>
2026-02-04 23:00:49 +0200

    fix: resolve parent destinations in sitemap without joins

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,18 +1,6 @@
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ locals }) => {
-	const { data: destinations } = await locals.supabase
-		.from('destinations')
-		.select('slug, type, updated_at, parent:destinations!parent_id(slug)')
-		.eq('published', true);
-
-	const { data: articles } = await locals.supabase
-		.from('articles')
-		.select(
-			'slug, updated_at, destination:destinations!destination_id(slug, parent:destinations!parent_id(slug))'
-		)
-		.eq('published', true);
-
 	const baseUrl = 'https://lomaopas.vercel.app';
 
 	let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
@@ -23,7 +11,19 @@ export const GET: RequestHandler = async ({ locals }) => {
     <priority>1.0</priority>
   </url>`;
 
+	const { data: destinations } = await locals.supabase
+		.from('destinations')
+		.select('id, slug, type, parent_id, updated_at')
+		.eq('published', true);
+
+	const { data: articles } = await locals.supabase
+		.from('articles')
+		.select('slug, updated_at, destination_id')
+		.eq('published', true);
+
 	if (destinations) {
+		const destMap = new Map(destinations.map((d) => [d.id, d]));
+
 		for (const dest of destinations) {
 			if (dest.type === 'country') {
 				sitemap += `
@@ -33,28 +33,39 @@ export const GET: RequestHandler = async ({ locals }) => {
     <changefreq>weekly</changefreq>
     <priority>0.8</priority>
   </url>`;
-			} else if (dest.type === 'city' && dest.parent) {
-				sitemap += `
+			}
+		}
+
+		for (const dest of destinations) {
+			if (dest.type === 'city' && dest.parent_id) {
+				const parent = destMap.get(dest.parent_id);
+				if (parent) {
+					sitemap += `
   <url>
-    <loc>${baseUrl}/${dest.parent.slug}/${dest.slug}</loc>
+    <loc>${baseUrl}/${parent.slug}/${dest.slug}</loc>
     <lastmod>${dest.updated_at}</lastmod>
     <changefreq>weekly</changefreq>
     <priority>0.7</priority>
   </url>`;
+				}
 			}
 		}
-	}
 
-	if (articles) {
-		for (const article of articles) {
-			if (article.destination?.parent) {
-				sitemap += `
+		if (articles) {
+			for (const article of articles) {
+				const city = destMap.get(article.destination_id);
+				if (city && city.parent_id) {
+					const country = destMap.get(city.parent_id);
+					if (country) {
+						sitemap += `
   <url>
-    <loc>${baseUrl}/${article.destination.parent.slug}/${article.destination.slug}/${article.slug}</loc>
+    <loc>${baseUrl}/${country.slug}/${city.slug}/${article.slug}</loc>
     <lastmod>${article.updated_at}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>0.6</priority>
   </url>`;
+					}
+				}
 			}
 		}
 	}
